b2cc01187a59f09f14e6b1e9146c949c6ee57a6c
Massimilian <vasalekmas@gmail.com>
2018-01-08 20:51:59 +0300

    5.2. Перевернуть массив. [#4441]

--- chapter_001/src/main/java/ru/job4j/array/Turn.java
+++ chapter_001/src/main/java/ru/job4j/array/Turn.java
@@ -1,11 +1,4 @@
-/**
 package ru.job4j.array;
-
-import org.junit.Test;
-import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.*;
-
-*/
 
 /**
  * @author Vasily Maslov (vasalekmas@gmail.com)
@@ -13,23 +6,18 @@ import static org.junit.Assert.*;
  * @since 08/01/2018
  */
  
- /**
+ 
  public class Turn {
 	 public int[] back(int[] array) {
 		 // {1, 2, 3, 4, 5}
 		 // {5, 4, 3, 2, 1}
 		 
-		 // 4, 5
-		 // 4 = 4+5; x = x+y
-		 // 5 = 4(9)-5; y = x-y
-		 // 4 = 4(9) - 5(4); x = x-y;
-		 
 		 for (int i = 0; i < array.length / 2; i++) {
-		 array[i] = array[i] + array[array.length - i];
-		 array[array.length - i] = array[i] - array[array.length - i];
-		 arrai[i] =  array[i] - array[array.length - i];
+		 array[i] = array[i] + array[array.length - 1 - i];
+		 array[array.length - 1 - i] = array[i] - array[array.length - 1 - i];
+		 array[i] = array[i] - array[array.length - 1 - i];
 		 }
 		 return array;
 	 }
  }
- */
+ 
