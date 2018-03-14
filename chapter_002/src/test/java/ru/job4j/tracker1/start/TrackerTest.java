20f01b372101277baafd48d9f5217a337461f8f0
Massimilian <vasalekmas@gmail.com>
2018-03-15 01:33:14 +0300

    3. Используя класс StubInput написать тесты проверяющие поведение пользователя [#785] [#42416] NEW-2

--- chapter_002/src/test/java/ru/job4j/tracker1/start/TrackerTest.java
+++ chapter_002/src/test/java/ru/job4j/tracker1/start/TrackerTest.java
@@ -44,15 +44,44 @@ public class TrackerTest {
 		assertThat(tracker.getItem(0), is(tracker.findById(tracker.getItem(0).getId())));
 	}
 	
-	// Суть работы StubInput понял после "глобальной перезагрузки".
-	// Но теперь мне неясно - как заставить Stubinput "узнавать" формируемый по ходу дела id-номер конкретной заявки?
-	// Без id не получится сделать полностью автоматизированный тест.
-	// И - если мы проверяем метод, выводящий информацию на консоль - как можно считать с консоли выведенную информацию?
-
 	@Test
 	public void whenAddNewItemThenPutItInArrayAutoTesting() {
 		Input input = new StubInput(new String[]{"1", "Task", "Desc", "n"});
 		new StartUI(input, tracker).init();
 		assertThat(tracker.getItem(0).getName(), is("Task"));
+	}
+		
+	@Test
+	public void whenReplaceAnItemThenPutANewItemAutoTesting() {
+		tracker.add(item);
+		tracker.getItem(0).setId("0000");
+		Input input = new StubInput(new String[]{"2", "0000", "NewTask", "NewDesc", "n"});
+		new StartUI(input, tracker).init();
+		assertThat(tracker.getItem(0).getDescription(), is("NewDesc"));
+	}
+	
+	@Test
+	public void whenDeleteAnItemThenReturnNullAutoTesting() {
+		tracker.add(item);
+		tracker.getItem(0).setId("0000");
+		Input input = new StubInput(new String[]{"3", "0000", "n"});
+		new StartUI(input, tracker).init();
+		assertThat(tracker.getPosition(), is(0));
+	}
+	
+	@Test
+	public void whenAskItemsByNameThenReturnAllAutoTesting() {
+		Input input = new StubInput(new String[]{"1", "Name0", "Desc0", "y", "1", "Name1", "Desc1", "n"});
+		new StartUI(input, tracker).init();
+		assertThat(tracker.getItem(1).getName(), is("Name1"));
+	}
+	
+	@Test
+	public void whenAskTrackerByIdThenReturnItAutoTesting() {
+		tracker.add(item);
+		tracker.getItem(0).setId("0000");
+		Input input = new StubInput(new String[]{"6", "0000", "n"});
+		new StartUI(input, tracker).init();
+		assertThat(tracker.getItem(0).getId(), is("0000"));		
 	}
 }
