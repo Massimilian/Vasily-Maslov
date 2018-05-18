642e05a20c217fc3ec4c9d42677f0eb97bf21404
Massimilian <vasalekmas@gmail.com>
2018-05-18 13:37:00 +0300

    Кофемашина [#34741] NEW

--- chapter_002/src/test/java/ru/job4j/homeworks/coffeeMachine/CoffeeMachineTest.java
+++ chapter_002/src/test/java/ru/job4j/homeworks/coffeeMachine/CoffeeMachineTest.java
@@ -1,15 +1,15 @@
-// package ru.job4j.homeworks.coffeeMachine;
-//
-// import org.junit.Test;
-// import static org.hamcrest.Matchers.is;
-// import static org.junit.Assert.*;
-//
-//public class CoffeeMachineTest {
-//
-//	 @Test
-//	 public void whenEnterANumberThenReturnTheResult() {
-//	 CoffeeMachine cof = new CoffeeMachine();
-//	 int[] change = cof.changes(100, 82);
-//	 assertThat(change, is(new int[]{10, 5, 2, 1}));
-//	 }
-//}
+ package ru.job4j.homeworks.coffeemachine;
+
+ import org.junit.Test;
+ import static org.hamcrest.Matchers.is;
+ import static org.junit.Assert.*;
+
+public class CoffeeMachineTest {
+
+	 @Test
+	 public void whenEnterANumberThenReturnTheResult() {
+	 CoffeeMachine cof = new CoffeeMachine();
+	 int[] change = cof.changes(100, 82);
+	 assertThat(change, is(new int[]{10, 5, 2, 1}));
+	 }
+}
