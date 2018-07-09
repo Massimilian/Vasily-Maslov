4a56b8839b320c70ec7a212d993aceaf3cf81a54
Massimilian <vasalekmas@gmail.com>
2018-07-09 17:06:03 +0300

    2. Set реализованный на связном списке. [#997][#60520] NEW

--- chapter_004/src/main/java/ru/job4j/set/SimpleLinkedSet.java
+++ chapter_004/src/main/java/ru/job4j/set/SimpleLinkedSet.java
@@ -9,22 +9,25 @@ public class SimpleLinkedSet<E> implements Iterable<E> {
     private DinamicLinkedContainer<E> dlc = new DinamicLinkedContainer<>();
 
     public void add(E e) {
-        boolean isEqual = false;
-        int max = dlc.getSize();
         if (dlc.getNode() == null) {
             dlc.add(e);
-        } else {
-            for (int i = 0; i < max; i++) {
-                if (e.equals(this.dlc.get(i))) {
-                    isEqual = true;
-                }
-            }
-            if (!isEqual) {
-                dlc.add(e);
-            }
+        }
+        if (!this.contains(e)) {
+            dlc.add(e);
         }
     }
 
+    private boolean contains(E value) {
+        boolean result = false;
+        int max = dlc.getSize();
+        for (int i = 0; i < max; i++) {
+            if (value.equals(this.dlc.get(i))) {
+                result = true;
+                break;
+            }
+        }
+        return result;
+    }
 
     public Iterator<E> iterator() {
         return dlc.iterator();
