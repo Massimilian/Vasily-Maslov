0b9da06016a72fb4f0dda9dabdb5bacbc4eee3ee
Massimilian <vasalekmas@gmail.com>
2018-09-19 16:30:51 +0300

    1. UML системы [#1093][#70252]

---
+++ chapter_004/src/main/java/ru/job4j/sql/uml-system/uml.sql
@@ -0,0 +1,54 @@
+﻿create database UML_systems;
+
+create table note (
+id serial primary key,
+name varchar(20),
+information text
+);
+
+create table attach (
+id serial primary key,
+name varchar(20),
+information text
+);
+
+create table state (
+id serial primary key,
+is_made boolean,
+remark text
+);
+
+create table category (
+id serial primary key,
+name varchar(20),
+decription text
+);
+
+create table rules (
+id serial primary key,
+name varchar(20)
+);
+
+create table roles (
+id serial primary key,
+name varchar(30),
+description text,
+rules_id int references rules(id)
+);
+
+create table users (
+id serial primary key,
+name varchar(30),
+roles_id integer references roles(id)
+);
+
+create table item (
+id serial primary key,
+name varchar(20),
+users_id int references users(id),
+comment_id int references note(id),
+attach_id int references attach(id),
+category_id int references category(id),
+state_is_id int references state(id)
+);
+
