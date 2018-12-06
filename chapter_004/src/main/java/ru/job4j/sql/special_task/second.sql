197ca94d7ea4627a6a4e9db6b92f632d315328f4
Massimilian <vasalekmas@gmail.com>
2018-12-07 02:00:29 +0300

    Extra Task

--- chapter_004/src/main/java/ru/job4j/sql/special_task/second.sql
+++ chapter_004/src/main/java/ru/job4j/sql/special_task/second.sql
@@ -3,8 +3,8 @@
 удалить все предполагаемые дубликаты из таблицы
 */
 
-CREATE TABLE second_table AS (SELECT DISTINCT ft.duplic FROM first_table as ft);  
-
-DROP TABLE first_table;
-
+CREATE TABLE second_table AS (SELECT DISTINCT ft.duplic FROM first_table as ft)
+UNION
+DROP TABLE first_table
+UNION
 ALTER TABLE second_table RENAME TO first_table;
