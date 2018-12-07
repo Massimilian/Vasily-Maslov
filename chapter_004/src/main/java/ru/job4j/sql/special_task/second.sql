9ab1dc9560fdcd10ab5a4a26bd025ed10d81b17a
Massimilian <vasalekmas@gmail.com>
2018-12-07 17:19:05 +0300

    Extra Task

--- chapter_004/src/main/java/ru/job4j/sql/special_task/second.sql
+++ chapter_004/src/main/java/ru/job4j/sql/special_task/second.sql
@@ -8,3 +8,6 @@ UNION
 DROP TABLE first_table
 UNION
 ALTER TABLE second_table RENAME TO first_table;
+
+DELETE FROM a USING (SELECT MIN(ctid) as ctid, name from a GROUP BY name HAVING COUNT(*) > 1 )
+b WHERE a.name = b.name AND a.ctid <> b.ctid;
