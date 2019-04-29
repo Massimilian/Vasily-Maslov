fcb088f1a60f11b31066f407bc56ef61a220e8ea
Massimilian <vasalekmas@gmail.com>
2019-04-29 20:54:47 +0300

    1. Исправить код класс SimpleGenerator. [#855][#114921]

--- chapter_006/src/main/java/ru/job4j/simplegenerator/SimpleRegexGenerator.java
+++ chapter_006/src/main/java/ru/job4j/simplegenerator/SimpleRegexGenerator.java
@@ -9,30 +9,15 @@ import java.util.regex.Pattern;
 public class SimpleRegexGenerator extends SimpleGenerator {
 
     public SimpleRegexGenerator() {
-        map = new HashMap<String, String>();
+        this.map = new HashMap<String, String>();
     }
 
-    @Override
-    public void addMapValue(ArrayList<String> values) {
-        map.put(values.get(0), values.get(1));
-    }
-
-    @Override
-    public void addMapValue(Map.Entry<String, String> entry) {
-        map.put(entry.getKey(), entry.getValue());
-    }
-
-    @Override
     public String generate(String enter) {
-        Pattern pattern;
-        Iterator<Map.Entry<String, String>> iterator = map.entrySet().iterator();
-        while (iterator.hasNext()) {
-            Map.Entry<String, String> entry = iterator.next();
-            pattern = Pattern.compile("\\$\\{" + entry.getKey() + "\\}");
-            Matcher matcher = pattern.matcher(enter);
-            while (matcher.find()) {
-                enter = enter.replace(String.format("%s%s%s", begin, entry.getKey(), end), entry.getValue());
-            }
+        Pattern pattern = Pattern.compile("\\$\\{.+?\\}");
+        Matcher matcher = pattern.matcher(enter);
+        while (matcher.find()) {
+            // Зачем компилятор потребовал добавления CharSequence? Ведь метод map.get() в любом случае вернёт String.
+            enter = enter.replace(matcher.group(), (CharSequence) map.get(matcher.group()));
         }
         return enter;
     }
