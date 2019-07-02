b2c5526e7652438664473a9dbfa7e7cdbdac8d97
Massimilian <vasalekmas@gmail.com>
2019-07-02 23:21:30 +0300

    1. Collection API Улучшения [#70623]

---
+++ chapter_006/src/main/java/ru/job4j/lambda/Lamb.java
@@ -0,0 +1,32 @@
+package ru.job4j.lambda;
+
+//import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils;
+
+import java.util.List;
+
+public class Lamb {
+    public static void main(String[] args) {
+//        List<Person> persons = new ArrayList<>();
+//        persons.add(new Person("Vasia", 36));
+//        persons.add(new Person("Olga", 36));
+//        persons.add(new Person("Vlad", 9));
+//        persons.add(new Person("Arina", 6));
+        List<Person> persons = List.of(new Person("Vasia", 35), new Person("Olga", 35), new Person("Vlad", 9), new Person("Arina", 6));
+
+        //Map<String, Integer> map = persons.stream().collect(Collectors.toMap(s -> s.getName(), b -> b.getAge()));
+        //persons.stream().map(x -> x.getName().toUpperCase()).forEach(x -> System.out.println(x));
+        //Stream.of(asList("a"), persons).flatMap(x -> x.stream()).forEach(System.out::println);
+        //Stream<Person> stream = persons.stream();
+        //persons.stream().forEach(System.out::println);
+        //List<Person> list = persons.stream().filter(p -> p.getAge() < 18).collect(Collectors.toList());
+        //persons.forEach(person -> System.out.println(person));
+        //persons.forEach(System.out::println);
+        //persons.stream().filter(p -> p.getAge() >= 18).forEach(p -> System.out.println(p.getName()));
+        //persons.stream().map(p -> p.getName()).forEach(System.out::println);
+        //persons.stream().sorted((c1, c2) -> c1.getName().compareTo(c2.getName())).forEach(p -> System.out.println(p.getName() + " " + p.getAge()));
+        //System.out.println(Stream.of(0, 9, 1, 2, 3, 4, 5).max(Comparator.comparing(x -> x)).get());
+        //int r = Stream.of(1, 2, 3, 4).reduce(1, (one, two) -> one * two);
+        //System.out.println(Stream.of("a", "b", "c").limit(200).collect(Collectors.toList()));
+
+    }
+}
--- chapter_006/src/test/java/ru/job4j/lambda/stream/LevelOfTest.java
+++ chapter_006/src/test/java/ru/job4j/lambda/stream/LevelOfTest.java
@@ -2,16 +2,14 @@ package ru.job4j.lambda.stream;
 
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
 public class LevelOfTest {
-    private Student n = null;
-    private List<Student> students = new ArrayList<Student>(Arrays.asList(new Student("Three", 3), new Student("Five", 5), new Student("Two", 2), new Student("Four", 4), new Student("One", 1), n));
+    //    private List<Student> students = new ArrayList<Student>(Arrays.asList(new Student("Three", 3), new Student("Five", 5), new Student("Two", 2), new Student("Four", 4), new Student("One", 1), n));
+    private List<Student> students = List.of(new Student("Three", 3), new Student("Five", 5), new Student("Two", 2), new Student("Four", 4), new Student("One", 1));
     private LevelOf test = new LevelOf();
 
     @Test
