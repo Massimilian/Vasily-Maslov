bd9d89ebdd2f99ddf837a71fd604479973dd1300
Massimilian <vasalekmas@gmail.com>
2019-07-16 19:34:08 +0300

    1. класс Count и метод int incremant() [#1102]

---
+++ chapter_007/src/test/java/ru.job4j/count/CountTest.java
@@ -0,0 +1,36 @@
+package ru.job4j.count;
+
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class CountTest {
+    /**
+     * Класс описывает нить со счетчиком.
+     */
+    private class ThreadCount extends Thread {
+        private final Count count;
+
+        private ThreadCount(final Count count) {
+            this.count = count;
+        }
+
+        @Override
+        public void run() {
+            this.count.increment();
+        }
+    }
+
+    @Test
+    public void whenExecute2ThreadThen2() throws InterruptedException {
+        final Count count = new Count(); //Создаем счетчик.
+        Thread first = new ThreadCount(count); //Создаем нити.
+        Thread second = new ThreadCount(count);
+        first.start();//Запускаем нити.
+        second.start();
+        first.join(); //Заставляем главную нить дождаться выполнения наших нитей.
+        second.join();
+        assertThat(count.get(), is(2)); //Проверяем результат.
+    }
+}
