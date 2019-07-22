51c4a5d1b84f3c646b94c55f15400d0e3d12e6d0
Massimilian <vasalekmas@gmail.com>
2019-07-22 11:20:33 +0300

    1. Реализовать ThreadPool [1099]

---
+++ chapter_007/src/main/java/ru.job4j/pool/ThreadPool.java
@@ -0,0 +1,38 @@
+package ru.job4j.pool;
+
+import ru.job4j.producerconsumer.SimpleBlockingQueue;
+
+import java.util.LinkedList;
+import java.util.List;
+
+// Не уверен, что верно понял задачу. Поэтому комментирую каждый шаг, чтобы было понятно, чего я пытаюсь добиться каждым следующим методом.
+public class ThreadPool {
+    private final int size = Runtime.getRuntime().availableProcessors();
+    private final List<Thread> threads = new LinkedList<>();
+    private final SimpleBlockingQueue<Runnable> tasks = new SimpleBlockingQueue<>();
+
+    // добавляем задачи в tasks. Если задач уже слишком много, tasks ждёт освобождения пространства. Затем запускаем метод run.
+    public void work(Runnable job) {
+        tasks.offer(job);
+        this.run();
+    }
+
+    // будим все потоки. Если размер threads не больше количества ядер в процессоре, то добавляем задачу (одновременно изымая её из tasks и освобождая место для следующей задачи). Иначе ждём.
+    private void run() {
+        notifyAll();
+        if (this.threads.size() < this.size) {
+            threads.add(new Thread(tasks.poll()));
+        } else {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    // по реализации данного метода не нашёл никаких указаний.
+    public void shutdown() {
+
+    }
+}
