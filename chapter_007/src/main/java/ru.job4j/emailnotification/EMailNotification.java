e4b6e45d89f84748224b3dedd64d44ce4df91e5b
Massimilian <vasalekmas@gmail.com>
2019-07-30 10:03:53 +0300

    ExecutorService рассылка почты. [63097][#162007]

--- chapter_007/src/main/java/ru.job4j/emailnotification/EMailNotification.java
+++ chapter_007/src/main/java/ru.job4j/emailnotification/EMailNotification.java
@@ -4,7 +4,7 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class EMailNotification {
-    final private ExecutorService pool = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+    private final ExecutorService pool = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
 
     public void emailTo(User user) {
         String subject = String.format("Notification %s to e-mail %s.", user.getUserName(), user.getEmail());
